74d62961d04334da09c86ef28eaa2b68ba5724c1
bivenst <tbivens320@gmail.com>
2017-01-13 09:40:11 -0500

    JavaTest project

--- src/logic/creditcard/validation/CreditCardValidatorFactory.java
+++ src/logic/creditcard/validation/CreditCardValidatorFactory.java
@@ -3,17 +3,16 @@ package logic.creditcard.validation;
 import logic.creditcard.validation.validators.CreditCardValidator;
 import logic.creditcard.validation.validators.MasterCardValidator;
 import logic.creditcard.validation.validators.VisaValidator;
-import model.CreditCard;
+import model.enums.CreditCardType;
 
 public class CreditCardValidatorFactory {
-    public static CreditCardValidator get(CreditCard creditCard) {
-        switch (creditCard.getCreditCardType()) {
-            case VISA:
-                return new VisaValidator();
-            case MASTERCARD:
-                return new MasterCardValidator();
-            default:
-                return null;
+    public static CreditCardValidator get(CreditCardType creditCardType) {
+        if (creditCardType == CreditCardType.VISA) {
+            return new VisaValidator();
+        } else if (creditCardType == CreditCardType.MASTERCARD) {
+            return new MasterCardValidator();
+        } else {
+            return null;
         }
     }
 }
--- test/logic/creditcard/validation/CreditCardValidatorFactoryTest.groovy
+++ test/logic/creditcard/validation/CreditCardValidatorFactoryTest.groovy
@@ -2,18 +2,12 @@ package logic.creditcard.validation
 
 import logic.creditcard.validation.validators.MasterCardValidator
 import logic.creditcard.validation.validators.VisaValidator
-import model.CreditCard
 import model.enums.CreditCardType
 
 class CreditCardValidatorFactoryTest extends GroovyTestCase {
     void testGet() {
-        assertEquals(VisaValidator.class, CreditCardValidatorFactory.get(setupCreditCard(CreditCardType.VISA)).class)
-        assertEquals(MasterCardValidator.class, CreditCardValidatorFactory.get(setupCreditCard(CreditCardType.MASTERCARD)).class)
+        assertEquals(VisaValidator.class, CreditCardValidatorFactory.get(CreditCardType.VISA).class)
+        assertEquals(MasterCardValidator.class, CreditCardValidatorFactory.get(CreditCardType.MASTERCARD).class)
+        assertNull(CreditCardValidatorFactory.get(null))
      }
-
-    private static CreditCard setupCreditCard(CreditCardType creditCardType) {
-        return new CreditCard() {{
-            setCreditCardType(creditCardType)
-        }}
-    }
 }
